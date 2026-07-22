3a92b4e3d70fafe787c26cf508ede4e4634ab1e7
naappe <naappe@gmail.com>
2026-07-22 15:37:53 +0500

    Remove retired stock utility pages from PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'white-saffron-pwa-v17';
+const CACHE_NAME = 'white-saffron-pwa-v18';
 const OFFLINE_URL = './offline.html';
 const APP_SHELL = [
-  './','./index.html','./supply-rates.html','./prices.html','./master.html','./stock.html','./setup-stock.html',
-  './brand-system.css','./pwa-install.js','./session-timeout.js','./manifest.webmanifest',OFFLINE_URL,
+  './','./index.html','./supply-rates.html','./prices.html','./master.html',
+  './brand-system.css','./pwa-install.js','./manifest.webmanifest',OFFLINE_URL,
   './assets/pwa/icon-192.svg','./assets/pwa/icon-512.svg','./assets/pwa/icon-maskable.svg'
 ];
 
