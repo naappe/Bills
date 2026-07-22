515f87de275ac794036ad8b5c41e859131f0434e
naappe <naappe@gmail.com>
2026-07-22 11:54:16 +0500

    Refresh PWA cache for semantic structure fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'white-saffron-pwa-v13';
+const CACHE_NAME = 'white-saffron-pwa-v14';
 const OFFLINE_URL = './offline.html';
 const APP_SHELL = [
   './','./index.html','./supply-rates.html','./prices.html','./master.html','./stock.html','./setup-stock.html',
