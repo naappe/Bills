2fac00b6be52d95121c7a22936b7ecb94e2fcaee
naappe <naappe@gmail.com>
2026-07-22 16:19:36 +0500

    Refresh PWA cache for responsive Bills layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'white-saffron-pwa-v25';
+const CACHE_NAME = 'white-saffron-pwa-v26';
 const OFFLINE_URL = './offline.html';
 const APP_SHELL = [
   './','./index.html','./supply-rates.html','./prices.html','./master.html',
