77c139d961b5e05b775840443d4efc74a711d00b
naappe <naappe@gmail.com>
2026-07-22 16:10:24 +0500

    Refresh PWA cache for Ocean Breeze layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'white-saffron-pwa-v22';
+const CACHE_NAME = 'white-saffron-pwa-v23';
 const OFFLINE_URL = './offline.html';
 const APP_SHELL = [
   './','./index.html','./supply-rates.html','./prices.html','./master.html',
