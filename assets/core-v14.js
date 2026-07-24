17061f9b283d694c5b05ec395e19a1fab89bdd1c
naappe <naappe@gmail.com>
2026-07-24 16:43:31 +0500

    Fix validation by restoring core-v14 compatibility asset

---
+++ assets/core-v14.js
@@ -0,0 +1,5 @@
+(()=>{'use strict';
+// Compatibility shim for older index.html revisions.
+// Core application logic is currently defined in index.html and modular assets.
+window.__WS_CORE_V14_COMPAT__=true;
+})();
