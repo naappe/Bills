f0e0a1c114a7c90553480b9cd6b78e84bfcc67f0
naappe <naappe@gmail.com>
2026-07-26 14:01:56 +0500

    Fix procurement login alias

--- app/js/config.js
+++ app/js/config.js
@@ -3,6 +3,12 @@ export const CONFIG={
   supabaseKey:'sb_publishable_LAn1liS2zqMqlB33IQJxIw_NbgWKix1',
   table:'bills',
   currency:'MVR',
-  loginAliases:{white:'whitesaffron2025@gmail.com',staff:'whitesaffron2025@gmail.com',supply:'whitesaffron20255@gmail.com',admin:'naappe@gmail.com',naappe:'naappe@gmail.com'},
+  loginAliases:{
+    white:'whitesaffron2025@gmail.com',
+    staff:'whitesaffron2025@gmail.com',
+    supply:'whitesaffron2025@gmail.com',
+    admin:'naappe@gmail.com',
+    naappe:'naappe@gmail.com'
+  },
   adminIds:['5c0d47f8-68c1-4a60-a1b8-c80885c385da']
 };
